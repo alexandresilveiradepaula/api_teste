76af9978e18475459eb4296f60449b5687285a41
alexandresilveiradepaula <silveiradepaula.alexandre@gmail.com>
2026-09-23 14:23:33 -0300

    Conclusão da criação do User Controller

--- src/main/java/api_teste/ds/controllers/UserController.java
+++ src/main/java/api_teste/ds/controllers/UserController.java
@@ -36,19 +36,29 @@ public class UserController {
     @GetMapping ("/{id}") // Mapeia requisições HTTP GET na rota "/user/{id}"
     public ResponseEntity<User> findById(@PathVariable Long Id){ // Método para buscar usuario por id capturado da URL
         User obj=this.userService.findById(Id); // Invoca a busca do usuário através do ID recebido
-        return ResponseEntity.ok().body(obj) // Retorn código HTTP 200(ok) com o onjeto User no corpo da resposta
+        return ResponseEntity.ok().body(obj); // Retorn código HTTP 200(ok) com o onjeto User no corpo da resposta
     } // Fim do método FindbyId
 
-    @PostMapping 
-    public ResponseEntity<Void> create(@Validated (CreateUser.class) @RequestBody User obj){
-        this.userService.create(obj);
-        URI url = ServletUriComponentsBuilder.fromCurrentRequest()
-            .path("/{id}").buildAndExpand(obj.getId()).toUri();
-            return ResponseEntity.created(url).build();
+    @PostMapping // Mapeia requisições HTTP POST  na rota base"/user"(criação de nocvo usuário)
+    public ResponseEntity<Void> create(@Validated (CreateUser.class) @RequestBody User obj){ // Valida regra de CreateUser e desserializa o corpo JSON
+        this.userService.create(obj); // Chama a camada de serviço para persistir o novo usuario no banco de dados. 
+        URI url = ServletUriComponentsBuilder.fromCurrentRequest() // Obtém a rota da requisição atual 
+            .path("/{id}").buildAndExpand(obj.getId()).toUri(); // Adiciona o Id do usuario gerado no final do caminho da URI
+            return ResponseEntity.created(url).build(); //Retorna código HTTP 201(created) contendo a URL no cabeçalho location
     }
 
-    
+    @PutMapping("/{id}")// Mapeia requisições HTTP PUT na rota base "/user/{id}" (atualização do usuário)
+    public ResponseEntity<Void> update(@Validated(UpdateUser.class)@RequestBody User obj, @PathVariable Long id){ // Aplica a regra de Updateuser e recebe ID e JSON
+        obj.setId(id); // Garante que o ID do objeto a ser atualizado corresponde ao ID informado no parametro da URL
+        this.userService.update(obj); // Executa a atualização da senha do usuário no banco de dados
+        return ResponseEntity.noContent().build();//Retorno código HTTP 204(No content)indicando sucesso sem corpo de resposta
+    }
 
+    @DeleteMapping ("/{id}") //mapeia requisições HTTP DELETE na rota "/user/{id}" (exclusão de usuário)
+    public ResponseEntity<Void> delete(@PathVariable Long id){ // Captura o ID da URL a ser deletado
+        this.userService.delete(id); // Invoca o método de deleção so serviço
+        return ResponseEntity.noContent().build();//Retorna códigoHTTP 204 (no content) confirmando a exclusão
+            }
     }
     
 
